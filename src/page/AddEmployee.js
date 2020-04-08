b21c03afeb1a3aadc23132fef3f14ee46b7aa112
Adella Fitriany Cahyana <adellafitriany28@gmail.com>
2020-04-08 21:36:40 +0700

    Add the profile page and improve Add Employee

--- src/page/AddEmployee.js
+++ src/page/AddEmployee.js
@@ -1,25 +1,205 @@
+// import React, { Component } from 'react';
+// import "semantic-ui-css/semantic.min.css";
+// import { Form, Button, Divider, Icon, Header } from "semantic-ui-react";
+// import axios from 'axios';
+
+// const GenderOptions = [
+//     { key: 'm', text: 'Male', value: 'male' },
+//     { key: 'f', text: 'Female', value: 'female' },
+//   ]
+
+// const BloodType = [
+//     {key: 'a', text: 'A', value: 'a'},
+//     {key: 'b', text: 'B', value: 'b'},
+//     {key: 'o', text: 'O', value: 'o'},
+//     {key: 'ab', text: 'AB', value: 'ab'}
+// ]
+
+// const statusOptions = [
+//     { key: 'y', text: 'Active', value: 'active' },
+//     { key: 'n', text: 'Inactive', value: 'inactive' },
+// ]
+
+
+// class AddEmployee extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+// 			employeeId: '',
+//             firstName: '',
+// 			lastName: '',
+// 			gender: '',
+// 			photos: '',
+//             phone: '',
+//             familyName: '',
+//             familyNumber:'',
+//             bloodType: '',
+//             hiredDate: '',
+//             deptId:'',
+//             status:''
+// 		}
+//     }
+//     changeHandler = e => {
+// 		this.setState({ [e.target.name]: e.target.value })
+//     }
+//     submitHandler = e => {
+// 		e.preventDefault()
+// 		console.log(this.state)
+// 		axios
+// 			.post('http://192.168.43.122:4000/employee', this.state)
+// 			.then(response => {
+// 				console.log(response)
+// 			})
+// 			.catch(error => {
+// 				console.log(error)
+// 			})
+// 	}
+//     render() {
+//         const { employeeId, firstName, lastName,gender,photos,phone,familyName,familyNumber,bloodType,hiredDate,
+//             deptId , status } = this.state
+//         return (
+//             <div>
+//                 <Divider horizontal>
+//                     <Header as='h4'>
+//                         <Icon name='user' />
+//                         Add New Employee
+//                     </Header>
+//                 </Divider>
+//                 <Form>
+//                 <Form.Field>
+//                         <label>Id </label>
+//                         <input name="employeeId" value={employeeId}  onChange={this.changeHandler} />
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <label>Photo </label>
+//                         <input type='file'name="photos" value={photos}  onChange={this.changeHandler} />
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <label>First Name </label>
+//                         <input placeholder='First Name' name="firstName" value={firstName}  onChange={this.changeHandler} />
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <label>Last Name </label>
+//                         <input placeholder='First Name' name="lastName" value={lastName}  onChange={this.changeHandler}/>
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <label>Phone </label>
+//                         <input placeholder='First Name' name="phone" value={phone}  onChange={this.changeHandler}/>
+//                     </Form.Field>
+//                     <Form.Select
+//                         fluid
+//                         label='Gender'
+//                         name="gender"
+//                         options={GenderOptions}
+//                         placeholder='Gender'
+//                         value={gender}
+//                         onChange={this.changeHandler}                       
+
+//                     />
+//                     <Form.Select
+//                         fluid
+//                         label='Blood Type'
+//                         name="bloodType"
+//                         options={BloodType}
+//                         placeholder='Blood Type'
+//                         value={bloodType}
+//                         onChange={this.changeHandler}
+//                     />
+//                     <Form.Select
+//                         fluid
+//                         label='Status'
+//                         name="status"
+//                         options={statusOptions}
+//                         placeholder='Blood Type'
+//                         value={status}
+//                         onChange={this.changeHandler}
+//                     />                 
+//                     <Form.Field>
+//                         <label>Hired Date </label>
+//                         <input placeholder='Hired Date' name="hiredDate" value={hiredDate} onChange={this.changeHandler}/>
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <label>Family Name </label>
+//                         <input placeholder='Family Name' name="familyName" value={familyName}  onChange={this.changeHandler} />
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <label>Family Number </label>
+//                         <input placeholder='Family Name' name="familyNumber" value={familyNumber}  onChange={this.changeHandler} />
+//                     </Form.Field>
+//                     <Button onClick={this.submitHandler} value="Submit">Submit</Button>
+//                 </Form>
+//             </div>
+
+//         );
+//     }
+// }
+// export default AddEmployee;
+
 import React, { Component } from 'react';
 import "semantic-ui-css/semantic.min.css";
-import { Form, Button, Divider, Icon, Header } from "semantic-ui-react";
+import { Form,  Divider, Icon, Header } from "semantic-ui-react";
+import axios from 'axios';
 
-const GenderOptions = [
-    { key: 'm', text: 'Male', value: 'male' },
-    { key: 'f', text: 'Female', value: 'female' },
-  ]
+// const GenderOptions = [
+//     { key: 'm', text: 'Male', value: 'male' },
+//     { key: 'f', text: 'Female', value: 'female' },
+//   ]
 
-const BloodType = [
-    {key: 'a', text: 'A', value: 'a'},
-    {key: 'b', text: 'B', value: 'b'},
-    {key: 'o', text: 'O', value: 'o'},
-    {key: 'ab', text: 'AB', value: 'ab'}
-]
+// const BloodType = [
+//     {key: 'a', text: 'A', value: 'a'},
+//     {key: 'b', text: 'B', value: 'b'},
+//     {key: 'o', text: 'O', value: 'o'},
+//     {key: 'ab', text: 'AB', value: 'ab'}
+// ]
 
-const status = [
-    { key: 'y', text: 'Active', value: 'active' },
-    { key: 'n', text: 'Inactive', value: 'inactive' },
-  ]
+// const Status = [
+//     { key: 'y', text: 'Active', value: 'active' },
+//     { key: 'n', text: 'Inactive', value: 'inactive' },
+//   ]
 class AddEmployee extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            employee: {
+                firstName: '',
+                lastName: '',
+                gender: '',
+                photos: '',
+                phone: '',
+                familyName: '',
+                familyNumber: '',
+                bloodType: '',
+                hiredDate: '',
+                deptId: '',
+                status: ''
+            }
+            
+		}
+    }
+    changeHandler = e => {
+        this.setState({ [e.target.name]: e.target.value })
+        
+	}
+
+    submitHandler = e => {
+		e.preventDefault()
+		console.log(this.state)
+		axios
+			.post('http://192.168.43.122:4000/employee', this.state)
+			.then(response => {
+				console.log(response)
+			})
+			.catch(error => {
+				console.log(error)
+            })
+
+        
+        
+	}
     render() {
+        // const {  firstName, lastName,gender,photos,phone,familyName,familyNumber,bloodType,hiredDate,
+        // deptId , status } = this.state
         return (
             <div>
                 <Divider horizontal>
@@ -28,46 +208,54 @@ class AddEmployee extends Component {
                         Add New Employee
                     </Header>
                 </Divider>
-                <Form>
+                <Form onSubmit={this.submitHandler}>
+                    <Form.Field>
+                        <label>Photos </label>
+                        <input type="file" placeholder='photos' name="photos" value={this.photos}  onChange={this.changeHandler}/>
+                    </Form.Field>
                     <Form.Field>
                         <label>First Name </label>
-                        <input placeholder='First Name' />
+                        <input placeholder='First Name' name="firstName" value={this.firstName}  onChange={this.changeHandler} />
                     </Form.Field>
                     <Form.Field>
                         <label>Last Name </label>
-                        <input placeholder='First Name' />
+                        <input placeholder='Last Name' name="lastName" value={this.lastName}  onChange={this.changeHandler}/>
                     </Form.Field>
                     <Form.Field>
                         <label>Phone </label>
-                        <input placeholder='First Name' />
+                        <input placeholder='Phone Number' name="phone" value={this.phone}  onChange={this.changeHandler}/>
                     </Form.Field>
-                    <Form.Select
-                        fluid
-                        label='Gender'
-                        options={GenderOptions}
-                        placeholder='Gender'
-                    />
-                    <Form.Select
-                        fluid
-                        label='Blood Type'
-                        options={BloodType}
-                        placeholder='Blood Type'
-                    />
                     <Form.Field>
-                        <label>Salary</label>
-                        <input placeholder='First Name' />
+                        <label>Gender</label>
+                        <input placeholder='Gender' name="gender" value={this.gender}  onChange={this.changeHandler}/>
                     </Form.Field>
+                    <Form.Field>
+                        <label>BloodType</label>
+                        
+                        <input placeholder='Blood Type' name="bloodType" value={this.bloodType}  onChange={this.changeHandler}/>
+                    </Form.Field>
+                  
                     <Form.Field>
                         <label>Hired Date </label>
-                        <input placeholder='First Name' />
+                        <input placeholder='Tanggal join' name="hiredDate" value={this.hiredDate}  onChange={this.changeHandler}/>
                     </Form.Field>
-                    <Form.Select
-                        fluid
-                        label='Status'
-                        options={status}
-                        placeholder='Active / Inactive'
-                    />
-                    <Button type='submit'>Submit</Button>
+                    <Form.Field>
+                       <label>Status</label>
+                       <input placeholder='Active / Inactive' name='status' value={this.status}  onChange={this.changeHandler}/>
+                     </Form.Field>
+                     <Form.Field>
+                       <label>Family Name</label>
+                       <input placeholder='Active / Inactive' name='familyName' value={this.familyName}  onChange={this.changeHandler}/>
+                     </Form.Field>
+                     <Form.Field>
+                       <label>Family Number</label>
+                       <input placeholder='Active / Inactive' name='familyNumber' value={this.familyNumber}  onChange={this.changeHandler}/>
+                     </Form.Field>
+                     <Form.Field>
+                        <label>Dept Id</label>
+                        <input placeholder='Dept Id' name="deptId" value={this.deptId}  onChange={this.changeHandler}/>
+                    </Form.Field>
+                <input type="submit" value="Submit Data" className="btn btn-primary"/>
                 </Form>
             </div>
 
--- src/page/Employee.js
+++ src/page/Employee.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
-import { Table, Segment, Button, Input, Grid, Icon, Header, Divider, Modal } from "semantic-ui-react";
+import { Table, Segment, Button, Input, Grid, Icon, Header, Divider, Modal, Image } from "semantic-ui-react";
 import Nav from "./Nav";
 import AddEmployee from './AddEmployee';
+import axios from 'axios';
 
 class Employee extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            employeeData: []
+            employeeData: [],
+            employee: {
+                firstName: '',
+                lastName: '',
+                gender: '',
+                photos: '',
+                phone: '',
+                familyName: '',
+                familyNumber: '',
+                bloodType: '',
+                hiredDate: '',
+                department: {
+                    deptId: '',
+                    departementName: '',
+                },
+                status: ''
+            }
         }
+    }
+    getDataId = e => {
+        axios
+            .get(`http://192.168.43.122:4000/employee/${e.target.value}`)
+            .then(res => {
+                this.setState({
+                    employee: res.data
+                })
+                console.log(res)
+            });
+
     }
     componentDidMount(){
         fetch("http://192.168.43.122:4000/employee")
@@ -33,9 +61,9 @@ class Employee extends Component {
                 <Grid container columns={2} stackable>
                     <Grid.Column>
                         <Modal trigger={<Button primary icon labelPosition="left" size='tiny'><Icon name='add' />Add Employee </Button>}>
-                           <Modal.Content>
+                            <Modal.Content>
                                 <AddEmployee />
-                           </Modal.Content>
+                            </Modal.Content>
                         </Modal>
                         <Button color='teal' icon labelPosition="left" size='tiny'><Icon name='filter' />Filter</Button>
 
@@ -50,34 +78,84 @@ class Employee extends Component {
                             <Table.HeaderCell>Photo</Table.HeaderCell>
                             <Table.HeaderCell>Name</Table.HeaderCell>
                             <Table.HeaderCell>Phone</Table.HeaderCell>
+                            <Table.HeaderCell>Gender</Table.HeaderCell>
                             <Table.HeaderCell>Blood Type</Table.HeaderCell>
-                            <Table.HeaderCell>Salary</Table.HeaderCell>
                             <Table.HeaderCell>Hired Date</Table.HeaderCell>
-                            <Table.HeaderCell>Departement</Table.HeaderCell>
                             <Table.HeaderCell>Status</Table.HeaderCell>
+                            <Table.HeaderCell>Family Name</Table.HeaderCell>
+                            <Table.HeaderCell>Family Number</Table.HeaderCell>
+                            <Table.HeaderCell>Departement</Table.HeaderCell>
                             <Table.HeaderCell>Action</Table.HeaderCell>
-
                         </Table.Row>
                     </Table.Header>
 
-                {this.state.employeeData.map((dat, index)=>{
-                    return (
-                                  
-                    <Table.Body key={index}>
-                        <Table.Row>
-                            <Table.Cell>{dat.photos}</Table.Cell>
-                            <Table.Cell>{dat.firstName} {dat.lastName}</Table.Cell>
-                            <Table.Cell>{dat.phone}</Table.Cell>
-                            <Table.Cell>{dat.bloodGroup}</Table.Cell>
-                            <Table.Cell>{dat.salary}</Table.Cell>
-                            <Table.Cell>{dat.hiredDate}</Table.Cell>
-                            <Table.Cell>{dat.department['departmentName']}</Table.Cell>
-                            <Table.Cell>{dat.active}</Table.Cell>
-                            <Table.Cell><Button color='green' size='mini'>EDIT</Button></Table.Cell>
-                        </Table.Row>
-                    </Table.Body>      
-                    );
-                })}
+                    {this.state.employeeData.map((dat, index) => {
+                        return (
+
+                            <Table.Body key={index}>
+                                <Table.Row>
+                                    <Table.Cell>{dat.photos}</Table.Cell>
+                                    <Table.Cell>{dat.firstName} {dat.lastName}</Table.Cell>
+                                    <Table.Cell>{dat.phone}</Table.Cell>
+                                    <Table.Cell>{dat.gender}</Table.Cell>
+                                    <Table.Cell>{dat.bloodType}</Table.Cell>
+                                    <Table.Cell>{dat.hiredDate}</Table.Cell>
+                                    <Table.Cell>{dat.status}</Table.Cell>
+                                    <Table.Cell>{dat.familyName}</Table.Cell>
+                                    <Table.Cell>{dat.familyNumber}</Table.Cell>
+                                    <Table.Cell>{dat.department['departmentName']}</Table.Cell>
+                                    <Table.Cell>
+                                    <Modal trigger={<Button color="green" size='mini' value={dat.employeeId} onClick={this.getDataId}>View </Button>}>
+                                        <Modal.Content>
+                                        <div>
+                                            <Divider horizontal>
+                                                <Header as='h4'>
+                                                     <Icon name='user' />
+                                                     Employee Profile
+                                                </Header>
+                                            </Divider>
+                                                <Grid>
+                                                    <Grid.Column width={4}>
+                                                        <Image src={this.state.employee.photos}  />
+                                                    </Grid.Column>
+                                                        <Grid.Column width={9}>
+                                                            <Header as='h5'>Name</Header>
+                                                            {this.state.employee.firstName} {this.state.employee.lastName}
+                                                            <Header as='h5'>Phone</Header>
+                                                            {this.state.employee.phone}
+                                                            <Header as='h5'>Gender</Header>
+                                                            {this.state.employee.gender}
+                                                            <Header as='h5'>Blood Type</Header>
+                                                            {this.state.employee.bloodType}
+                                                            <Header as='h5'>Hired Date</Header>
+                                                            {this.state.employee.hiredDate}
+                                                            <Header as='h5'>status</Header>
+                                                            {this.state.employee.status}
+                                                            <Header as='h5'>Family Related</Header>
+                                                            {this.state.employee.familyName} {this.state.employee.familyNumber} 
+                                                            <Header as='h5'>Departement Name</Header>
+                                                            {this.state.employee.deptId}
+                                                        </Grid.Column>
+                                                    
+                                                    </Grid>
+                                                    
+                                                    
+                                        </div>
+                                        </Modal.Content>
+                                            <Modal.Actions>
+                                            <Button color='red'>
+                                                <Icon name='file pdf'/> Export
+                                            </Button>
+                                            <Button color='black'>
+                                                <Icon name='qrcode'/> Generate
+                                            </Button>
+                                            </Modal.Actions>
+                                    </Modal>
+                                    </Table.Cell>
+                                </Table.Row>
+                            </Table.Body>
+                        );
+                    })}
                 </Table>
             </div>
         )
